e46d138588f8f84a1072dd7983ffc56a6292153d
Vivvianne Kimani <ndiahav@gmail.com>
2018-07-05 15:22:36 +0300

    adding an image

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,21 @@ jQuery(document).ready(function() {
         alert("This is an image.");
     })
 
+    $('cipher').clicked(function(){
+        var input = prompt('string you want ciphered');
+
+        var capitaliseFirstLastReversed = function(string){
+            var capsFirst = string.charAt(0).toUpperCase();
+            var capsLast = string.charAt((string.length -1)).toUpperCase();
+            return capsLast + capsFirst;
+        }
+
+        var originalPlusReversedChar = function(string){
+            var reversedFirstLast = capitaliseFirstLastReversed(string);
+        }
+
+
+    });
+
 
 })
